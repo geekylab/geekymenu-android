c056a437b55b98bb4a786ff17710d6d5428758cb
johna1203 <johnathan@kodokuman.com>
2014-12-07 16:59:38 -0200

    ItemlistFragment extends AbstractBaseListFragment

--- app/src/main/java/com/geekylab/menu/geekymenutest/ItemListFragment.java
+++ app/src/main/java/com/geekylab/menu/geekymenutest/ItemListFragment.java
@@ -2,19 +2,11 @@ package com.geekylab.menu.geekymenutest;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.BaseAdapter;
 import android.widget.Toast;
-
 
 import com.geekylab.menu.geekymenutest.adapters.ItemAdapter;
 import com.geekylab.menu.geekymenutest.db.entity.ItemEntity;
@@ -34,16 +26,7 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.Locale;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Large screen devices (such as tablets) are supported by replacing the ListView
- * with a GridView.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnFragmentInteractionListener}
- * interface.
- */
-public class ItemListFragment extends Fragment implements
+public class ItemListFragment extends AbstractBaseListFragment implements
         AbsListView.OnItemClickListener,
         IFTaskCallback {
 
@@ -51,23 +34,9 @@ public class ItemListFragment extends Fragment implements
     private static final String ARG_STORE_ID = "store_id";
     private static final String TAG = ItemListFragment.class.getSimpleName();
     private final String defaultLanguage;
-
     private String mCategoryId;
     private String mStoreId;
-
     private OnFragmentInteractionListener mListener;
-
-    /**
-     * The fragment's ListView/GridView.
-     */
-    private AbsListView mListView;
-
-    /**
-     * The Adapter which will be used to populate the ListView/GridView with
-     * Views.
-     */
-    private ItemAdapter mAdapter;
-
     private ArrayList<ItemEntity> m_parts = new ArrayList<ItemEntity>();
 
     public static ItemListFragment newInstance(String store_id, String categoryId) {
@@ -79,10 +48,6 @@ public class ItemListFragment extends Fragment implements
         return fragment;
     }
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
     public ItemListFragment() {
         defaultLanguage = Locale.getDefault().getLanguage();
     }
@@ -101,23 +66,10 @@ public class ItemListFragment extends Fragment implements
         postParameters.add(new BasicNameValuePair("category_id", mCategoryId));
         new DownloadJsonAsyncTaskHelper(getActivity(), this, HttpPost.METHOD_NAME, postParameters)
                 .execute(url);
-
-        mAdapter = new ItemAdapter(getActivity(), R.layout.item_list_row, m_parts);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_itemlist, container, false);
-
-        // Set the adapter
-        mListView = (AbsListView) view.findViewById(android.R.id.list);
-        ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
-
-        // Set OnItemClickListener so we can be notified on item clicks
-        mListView.setOnItemClickListener(this);
-
-        return view;
+    public BaseAdapter getListAdapter() {
+        return new ItemAdapter(getActivity(), R.layout.item_list_row, m_parts);
     }
 
     @Override
@@ -144,19 +96,6 @@ public class ItemListFragment extends Fragment implements
             // Notify the active callbacks interface (the activity, if the
             // fragment is attached to one) that an item has been selected.
             mListener.onFragmentInteraction(DummyContent.ITEMS.get(position).id);
-        }
-    }
-
-    /**
-     * The default content for this Fragment has a TextView that is shown when
-     * the list is empty. If you would like to change the text, call this method
-     * to supply the text it should use.
-     */
-    public void setEmptyText(CharSequence emptyText) {
-        View emptyView = mListView.getEmptyView();
-
-        if (emptyView instanceof TextView) {
-            ((TextView) emptyView).setText(emptyText);
         }
     }
 
@@ -208,7 +147,7 @@ public class ItemListFragment extends Fragment implements
                         m_parts.add(itemEntity);
                     }
 
-                    mAdapter.notifyDataSetChanged();
+                    getListAdapter().notifyDataSetChanged();
                 } else {
                     String message = jsonObject.getString("message");
                     if (message == null)
@@ -221,16 +160,6 @@ public class ItemListFragment extends Fragment implements
         }
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         public void onFragmentInteraction(String id);
     }
