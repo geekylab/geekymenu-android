e239bf7ac4dc388f989c041c8c1e7a2e98e6f98c
johna1203 <johnathan@kodokuman.com>
2014-12-14 19:05:42 -0200

    Login Activity in FirstActivity.

--- app/src/main/java/com/geekylab/menu/geekymenutest/FirstActivity.java
+++ app/src/main/java/com/geekylab/menu/geekymenutest/FirstActivity.java
@@ -1,19 +1,15 @@
 package com.geekylab.menu.geekymenutest;
 
+import android.accounts.AccountManager;
 import android.app.Activity;
-import android.app.LoaderManager;
 import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.Loader;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -21,24 +17,20 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 import com.geekylab.menu.geekymenutest.network.GoogleTokenRestTask;
 import com.geekylab.menu.geekymenutest.network.RestTask;
 import com.geekylab.menu.geekymenutest.openapi.Params;
+import com.google.android.gms.common.AccountPicker;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
-import com.google.android.gms.plus.People;
-import com.google.android.gms.plus.Plus;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
@@ -52,16 +44,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class FirstActivity extends PlusBaseActivity implements LoaderManager.LoaderCallbacks<Cursor>, ResultCallback<People.LoadPeopleResult> {
+public class FirstActivity extends Activity {
 
     public static final String EXTRA_MESSAGE = "message";
     public static final String PROPERTY_REG_ID = "registration_id";
     private static final String PROPERTY_APP_VERSION = "appVersion";
     private static final String PROPERTY_USER_TOKEN = "user_token";
+    private static final String PROPERTY_SERVICE_TOKEN = "service_token";
     private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final int REQUEST_CODE_PICK_ACCOUNT = 1000;
     private static final int REQ_SIGN_IN_REQUIRED = 55664;
     private static final String GOOGLE_TOKEN_ACTION = "google_token_action";
     private static final String SEND_REGISTRATION_ID_ACTION = "SEND_REGISTRATION_ID_ACTION";
+
     private ProgressDialog progressDialog;
 
     /**
@@ -99,6 +94,7 @@ public class FirstActivity extends PlusBaseActivity implements LoaderManager.Loa
                 progressDialog.dismiss();
 
             String response = "";
+            //google token result
             if (intent.getAction().equals(GOOGLE_TOKEN_ACTION)) {
                 response = intent.getStringExtra(GoogleTokenRestTask.HTTP_RESPONSE);
                 if (response != null) {
@@ -111,18 +107,16 @@ public class FirstActivity extends PlusBaseActivity implements LoaderManager.Loa
                             JSONObject profileJsonObject = jsonObject.getJSONObject("profile");
                             mServiceToken = profileJsonObject.getString("service_token");
                         }
-
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
 
                     if (status && !mServiceToken.isEmpty()) {
-                        storeGoogleToken(context, mServiceToken);
-
+                        storeServiceToken(context, mServiceToken);
                         //register regId
                         gcm = GoogleCloudMessaging.getInstance(FirstActivity.this);
                         regid = getRegistrationId(context);
-                        if (true || regid.isEmpty()) {
+                        if (regid.isEmpty()) {
                             progressDialog = ProgressDialog.show(FirstActivity.this, "Registration service", "Waitng for results....");
                             registerInBackground();
                         }
@@ -154,10 +148,14 @@ public class FirstActivity extends PlusBaseActivity implements LoaderManager.Loa
             mPlusSignInButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    signIn();
+                    mServiceToken = getServiceToken(context);
+                    if (mServiceToken.isEmpty()) {
+                        pickUserAccount();
+                    } else {
+
+                    }
                 }
             });
-
         } else {
             // Don't offer G+ sign in if the app's version is too low to support Google Play
             // Services.
@@ -165,12 +163,37 @@ public class FirstActivity extends PlusBaseActivity implements LoaderManager.Loa
             return;
         }
 
+
+//        mPlusSignInButton = (SignInButton) findViewById(R.id.plus_sign_in_button);
+//        if (checkPlayServices()) {
+//            // Set a listener to connect the user when the G+ button is clicked.
+//            mPlusSignInButton.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    signIn();
+//                }
+//            });
+//
+//        } else {
+//            // Don't offer G+ sign in if the app's version is too low to support Google Play
+//            // Services.
+//            mPlusSignInButton.setVisibility(View.GONE);
+//            return;
+//        }
+
 //        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
 //        populateAutoComplete();
 
 
 //        mQrCodeButton = (Button) findViewById(R.id.read_qrcode);
 //        mQrCodeButton.setOnClickListener(this);
+    }
+
+    private void pickUserAccount() {
+        String[] accountTypes = new String[]{"com.google"};
+        Intent intent = AccountPicker.newChooseAccountIntent(null, null,
+                accountTypes, false, null, null, null, null);
+        startActivityForResult(intent, REQUEST_CODE_PICK_ACCOUNT);
     }
 
     // You need to do the Play Services APK check here too.
@@ -190,10 +213,6 @@ public class FirstActivity extends PlusBaseActivity implements LoaderManager.Loa
         unregisterReceiver(taskReceiver);
     }
 
-    private void populateAutoComplete() {
-        getLoaderManager().initLoader(0, null, this);
-    }
-
 
 //    @Override
 //    public void onClick(View v) {
@@ -209,56 +228,79 @@ public class FirstActivity extends PlusBaseActivity implements LoaderManager.Loa
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == REQUEST_CODE_PICK_ACCOUNT) {
+            // Receiving a result from the AccountPicker
+            if (resultCode == RESULT_OK) {
+                String accountName = intent.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                // With the account name acquired, go get the auth token
 
-        if (requestCode == SCANNER_REQUEST_CODE) {
-            // Handle scan intent
-            if (resultCode == Activity.RESULT_OK) {
-                // Handle successful scan
-                String contents = intent.getStringExtra("SCAN_RESULT");
-                String formatName = intent.getStringExtra("SCAN_RESULT_FORMAT");
-                byte[] rawBytes = intent.getByteArrayExtra("SCAN_RESULT_BYTES");
-                int intentOrientation = intent.getIntExtra("SCAN_RESULT_ORIENTATION", Integer.MIN_VALUE);
-                Integer orientation = (intentOrientation == Integer.MIN_VALUE) ? null : intentOrientation;
-                String errorCorrectionLevel = intent.getStringExtra("SCAN_RESULT_ERROR_CORRECTION_LEVEL");
-
-                try {
-                    Uri uri = Uri.parse(contents);
-                    if (uri.getHost().equals("menu.geekylab.net")) {
-                        List<String> pathSegments = uri.getPathSegments();
-                        if (pathSegments.size() >= 2) {
-                            if (pathSegments.get(0).equals("_store")) {
-
-                                String mStoreId = pathSegments.get(1);
-
-                                String mTableId = null;
-                                if (pathSegments.size() >= 3) {
-                                    mTableId = pathSegments.get(2);
-                                }
-
-                                //check table
-
-                                Intent menuIntent = new Intent(FirstActivity.this, MenuActivity.class);
-                                menuIntent.putExtra(ARG_STORE_ID, mStoreId);
-                                menuIntent.putExtra(ARG_TABLE_ID, mTableId);
-                                startActivity(menuIntent);
-
-
-//                                Log.d(TAG, pathSegments.get(1));
-//                                mSectionsPagerAdapter.notifyDataSetChanged();
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    Log.d(TAG, e.getMessage());
+                if (!accountName.isEmpty()) {
+                    GoogleTokenRestTask googleTokenRestTask =
+                            new GoogleTokenRestTask(context, GOOGLE_TOKEN_ACTION, accountName);
+                    googleTokenRestTask.execute();
+                    progressDialog = ProgressDialog.show(this, "Login", "Waitng for results....");
+                } else {
+                    Log.d(TAG, "Account Name is null");
                 }
 
-
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-                // Handle cancel
+//                getUsername();
+            } else if (resultCode == RESULT_CANCELED) {
+                // The account picker dialog closed without selecting an account.
+                // Notify users that they must pick an account to proceed.
+//                Toast.makeText(this, R.string.pick_account, Toast.LENGTH_SHORT).show();
             }
-        } else {
-            // Handle other intents
         }
+
+
+//        if (requestCode == SCANNER_REQUEST_CODE) {
+//            // Handle scan intent
+//            if (resultCode == Activity.RESULT_OK) {
+//                // Handle successful scan
+//                String contents = intent.getStringExtra("SCAN_RESULT");
+//                String formatName = intent.getStringExtra("SCAN_RESULT_FORMAT");
+//                byte[] rawBytes = intent.getByteArrayExtra("SCAN_RESULT_BYTES");
+//                int intentOrientation = intent.getIntExtra("SCAN_RESULT_ORIENTATION", Integer.MIN_VALUE);
+//                Integer orientation = (intentOrientation == Integer.MIN_VALUE) ? null : intentOrientation;
+//                String errorCorrectionLevel = intent.getStringExtra("SCAN_RESULT_ERROR_CORRECTION_LEVEL");
+//
+//                try {
+//                    Uri uri = Uri.parse(contents);
+//                    if (uri.getHost().equals("menu.geekylab.net")) {
+//                        List<String> pathSegments = uri.getPathSegments();
+//                        if (pathSegments.size() >= 2) {
+//                            if (pathSegments.get(0).equals("_store")) {
+//
+//                                String mStoreId = pathSegments.get(1);
+//
+//                                String mTableId = null;
+//                                if (pathSegments.size() >= 3) {
+//                                    mTableId = pathSegments.get(2);
+//                                }
+//
+//                                //check table
+//
+//                                Intent menuIntent = new Intent(FirstActivity.this, MenuActivity.class);
+//                                menuIntent.putExtra(ARG_STORE_ID, mStoreId);
+//                                menuIntent.putExtra(ARG_TABLE_ID, mTableId);
+//                                startActivity(menuIntent);
+//
+//
+////                                Log.d(TAG, pathSegments.get(1));
+////                                mSectionsPagerAdapter.notifyDataSetChanged();
+//                            }
+//                        }
+//                    }
+//                } catch (Exception e) {
+//                    Log.d(TAG, e.getMessage());
+//                }
+//
+//
+//            } else if (resultCode == Activity.RESULT_CANCELED) {
+//                // Handle cancel
+//            }
+//        } else {
+//            // Handle other intents
+//        }
     }
 
 
@@ -289,25 +331,26 @@ public class FirstActivity extends PlusBaseActivity implements LoaderManager.Loa
         return registrationId;
     }
 
-//    private String getUserToken(Context context) {
-//        final SharedPreferences prefs = getGCMPreferences(context);
-//        String registrationId = prefs.getString(PROPERTY_USER_TOKEN, "");
-//        if (registrationId.isEmpty()) {
-//            Log.i(TAG, "User token not found.");
-//            return "";
-//        }
-//
-//        // Check if app was updated; if so, it must clear the registration ID
-//        // since the existing regID is not guaranteed to work with the new
-//        // app version.
-//        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
-//        int currentVersion = getAppVersion(context);
-//        if (registeredVersion != currentVersion) {
-//            Log.i(TAG, "App version changed.");
-//            return "";
-//        }
-//        return registrationId;
-//    }
+    /**
+     * @param context Application context
+     * @return registrationId Geeky Menu Service Token
+     */
+    private String getServiceToken(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String serviceToken = prefs.getString(PROPERTY_SERVICE_TOKEN, "");
+        if (serviceToken.isEmpty()) {
+            Log.i(TAG, "service token not found.");
+            return "";
+        }
+
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+        return serviceToken;
+    }
 
     /**
      * @return Application's {@code SharedPreferences}.
@@ -436,14 +479,29 @@ public class FirstActivity extends PlusBaseActivity implements LoaderManager.Loa
      * Stores the registration ID and app versionCode in the application's
      * {@code SharedPreferences}.
      *
-     * @param context application's context.
-     * @param regId   registration ID
+     * @param context     application's context.
+     * @param googleToken registration ID
      */
-    private void storeGoogleToken(Context context, String regId) {
+    private void storeGoogleToken(Context context, String googleToken) {
         final SharedPreferences prefs = getGCMPreferences(context);
-        Log.i(TAG, "Saving serviceToken " + regId);
+        Log.i(TAG, "Saving google token " + googleToken);
         SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(PROPERTY_USER_TOKEN, regId);
+        editor.putString(PROPERTY_USER_TOKEN, googleToken);
+        editor.commit();
+    }
+
+    /**
+     * Stores the registration ID and app versionCode in the application's
+     * {@code SharedPreferences}.
+     *
+     * @param context      application's context.
+     * @param serviceToken registration ID
+     */
+    private void storeServiceToken(Context context, String serviceToken) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        Log.i(TAG, "Saving service Token " + serviceToken);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_SERVICE_TOKEN, serviceToken);
         editor.commit();
     }
 
@@ -462,93 +520,6 @@ public class FirstActivity extends PlusBaseActivity implements LoaderManager.Loa
         editor.putString(PROPERTY_REG_ID, regId);
         editor.putInt(PROPERTY_APP_VERSION, appVersion);
         editor.commit();
-    }
-
-    /**
-     * Login
-     */
-    @Override
-    protected void onPlusClientRevokeAccess() {
-
-    }
-
-    @Override
-    protected void onPlusClientSignIn() {
-
-        //get token
-        String accountName = Plus.AccountApi.getAccountName(getPlusClient());
-        if (!accountName.isEmpty()) {
-            GoogleTokenRestTask googleTokenRestTask =
-                    new GoogleTokenRestTask(context, GOOGLE_TOKEN_ACTION, accountName);
-            googleTokenRestTask.execute();
-            progressDialog = ProgressDialog.show(this, "Login", "Waitng for results....");
-        } else {
-            Log.d(TAG, "accountName is null");
-        }
-
-    }
-
-    @Override
-    protected void onPlusClientSignOut() {
-
-    }
-
-    @Override
-    protected void onPlusClientBlockingUI(boolean show) {
-
-    }
-
-    @Override
-    protected void updateConnectButtonState() {
-
-    }
-
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
-        List<String> emails = new ArrayList<String>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-
-    }
-
-    @Override
-    public void onResult(People.LoadPeopleResult loadPeopleResult) {
-
-    }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection) {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<String>(FirstActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-//        mEmailView.setAdapter(adapter);
     }
 
 }
